67bd407626949cac137b56fb48c297cb1fa0abda
Josiah Morris <josiahbmorris@hotmail.com>
2023-04-25 02:48:47 +0900

    Added head and tail to linkedList

--- linked_list.js
+++ linked_list.js
@@ -5,4 +5,18 @@ const node = () => {
   }
 }
 
-const linkedList = () => {}
+const linkedList = () => {
+  let head = node()
+  let tail = () => {
+    let curr = head
+    while (curr.next) {
+      curr = curr.next
+    }
+    return curr
+  }
+
+  return {
+    head,
+    tail,
+  }
+}
