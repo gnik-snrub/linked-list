3c412e1371fe8ec7d2f03ce9578ed1bb3bce733e
Josiah Morris <josiahbmorris@hotmail.com>
2023-04-25 04:05:37 +0900

    Added append, prepend, and size functions to linkedList

--- linked_list.js
+++ linked_list.js
@@ -1,3 +1,5 @@
+const util = require('util')
+
 const node = () => {
   return {
     value: null,
@@ -9,14 +11,47 @@ const linkedList = () => {
   let head = node()
   let tail = () => {
     let curr = head
-    while (curr.next) {
-      curr = curr.next
-    }
-    return curr
+    if (!curr) { return null }
+    while (curr.next) { curr = curr.next }
+    return curr 
   }
 
-  return {
-    head,
-    tail,
+  const append = (newData) => {
+    const newNode = node()
+    newNode.value = newData
+    if (!tail()) {
+      head = newNode
+    } else {
+      tail().next = newNode
+    }
   }
+
+  const prepend = (newData) => {
+    const newNode = node()
+    newNode.value = newData
+    newNode.next = head
+    head = newNode
+  }
+
+  const size = () => {
+    let count = head ? 1 : 0
+    let current = head
+    while (current.next) {
+      count++
+      current = current.next
+    }
+	  return count
+  }
+
+  const wrapper = {
+    get head() {
+      return head
+    },
+    tail,
+    prepend,
+    append,
+    size
+  }
+
+  return wrapper
 }
